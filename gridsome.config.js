7f3bd68ea2eaaf99239af8c4bff619a26f646ca6
Daniel Lerch <daniel@lerchpanther.ch>
2019-08-27 12:09:07 +0200

    starting with components

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,17 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Medin Biel Bienne',
+  plugins: [{
+    use: 'gridsome-source-graphql',
+    options: {
+      url: 'https://medinbackend.daniellerch.ch/api',
+      fieldName: 'medin',
+      typeName: 'medinTypes',
+
+      headers: {
+        Authorization: `Bearer gWrFJln9LH9o67J8XmJwBscsa2ij0ORuX5LUEUUhX3w8l52USH8dDS913l7b0B_P`,
+      },
+    },
+  }, ]
 }
